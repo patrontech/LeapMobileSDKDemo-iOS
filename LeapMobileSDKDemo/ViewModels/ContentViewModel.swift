9789dcc3b49004f0c2d9e482b2836448bf3c105f
Diego Cichello <diegocichelloarctouch@gmail.com>
2026-03-06 17:15:59 -0300

    Fixing the xcodeproj issues and testing it for Fanatics release

--- LeapMobileSDKDemo/ViewModels/ContentViewModel.swift
+++ LeapMobileSDKDemo/ViewModels/ContentViewModel.swift
@@ -118,9 +118,12 @@ final class ContentViewModel: NSObject, ObservableObject {
   }
   
   func logoutUser() {
+    /*
+     This will be uncommented as soon we have the new SDK on master
     Task {
       try await LeapMobileSDK.logoutUser()
     }
+    */
   }
   
   // MARK: - Notification Testing
